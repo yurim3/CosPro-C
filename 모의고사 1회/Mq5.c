2bc15843b5b3e2db86a8bebc8183c6ceaec2fb6d
yurim3 <w2103@e-mirim.hs.kr>
2023-01-04 13:29:48 +0900

    모의고사 1회-5 풀어보기(이해완료)

--- 모의고사 1회/Mq5.c
+++ 모의고사 1회/Mq5.c
@@ -5,12 +5,12 @@
 int solution(int number) {
 	int count = 0;
 	for (int i = 1; i <= number; i++) {
-		
+		int current = i;
 			while (current != 0) {
 				if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
-					
+					count++; //3 6 9일 때 count++
 				}
-				
+				current /= 10; //자릿수 바꾸는 코드(일의 자리면 자릿수를 줄일 필요 없음)
 			}
 		return count;
 	}
