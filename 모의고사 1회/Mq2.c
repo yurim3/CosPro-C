d800bb6cd091e739a4d36acd1e3acc4756a8378a
yurim3 <w2103@e-mirim.hs.kr>
2023-01-04 13:23:54 +0900

    모의고사 1회-2 풀어보기

---
+++ 모의고사 1회/Mq2.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int func_a(int month, int day) {
+	int month_list[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int total = 0;
+	for (int i = 0; i ; i++)
+		total += ;
+		total += day;
+	return total - 1;
+}
+
+int solution(int start_month, int start_day, int end_month, int
+	end_day) {
+	int start_total = func_a(start_month, start_day);
+	int end_total = ;
+	return ;
+}
