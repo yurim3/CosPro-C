6911a59b8117572c0129ebe625c3f3751027d5be
yurim3 <w2103@e-mirim.hs.kr>
2023-01-04 13:24:12 +0900

    모의고사 1회-5 풀어보기

---
+++ 모의고사 1회/Mq5.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int solution(int number) {
+	int count = 0;
+	for (int i = 1; i <= number; i++) {
+		
+			while (current != 0) {
+				if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+					
+				}
+				
+			}
+		return count;
+	}
