9338016813b4d78cdf47743f7b4fbf2b9f220f99
yurim3 <w2103@e-mirim.hs.kr>
2023-01-05 11:31:00 +0900

    cospro 기출문제3-2. 예산을 분배하기 위한 금액을 구하는 함수 빈칸 채우기

---
+++ 기출문제3-2/Q3-2.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int func_a(int arr[], int len) { //합계 구하는 함수
+	int total = 0;
+	for (int i = 0; i < len; ++i) {
+		total += arr[i];
+	}
+	return total;
+}
+
+int* solution(int total, int arr[], int arr_len) {
+	int* result = (int*)malloc(sizeof(int) * arr_len); //
+	int req_total = func_a(arr, arr_len); //신청한 예산의 total
+	if (req_total > total) { //신청한 값이 예산(1000)보다 크면
+		for (int i = 0; i < arr_len; ++i) {
+			result[i] = total / arr_len;
+		}
+	}
+	else {
+		for (int i = 0; i < arr_len; ++i) {
+			result[i] = arr[i];
+		}
+	}
+	return result;
+}
+
+int main(void) {
+	int req_arr[4] = { 250, 400, 200, 300 };
+	int arr_len = sizeof(req_arr) / sizeof(req_arr[0]);
+	int total = 1000;
+	int* result = solution(total, req_arr, arr_len);
+	for (int i = 0; i < arr_len; ++i) {
+		printf("[%d] ", result[i]);
+	}
+}
